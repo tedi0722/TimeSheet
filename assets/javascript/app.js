60440460ec4c61243d98ca8a1fcbb654c0466d0f
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2018-10-04 19:36:33 -0700

    wrote pseudo-code for displaying variable data from database to the current employee div

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// psuedo-code for displaying data to the current employees table.
+
+// #employee-name-display
+// #role-display
+// #start-date-display
+// #monthly-rate-display
+
+// traverse database using json to get relevent information.
+// using jquery target the proper ids for each element.
+// use .html() to add database variable data to the appropriate html elements.
